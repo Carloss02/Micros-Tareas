5b630280647d86fcd3117182f5b774f480cde477
cpirirm <cpirirm@miumg.edu.gt>
2022-09-26 21:05:45 -0600

    Subiendo archivos

---
+++ src/main/java/com/dase/gestortareas/commons/CommonSvcImpl.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dase.gestortareas.commons;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+/**
+ *
+ * @author cvsantos
+ */
+public class CommonSvcImpl<E, R extends CrudRepository<E, Object>> implements CommonSvc<E>{
+    
+    @Autowired
+    protected R repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Iterable<E> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<E> findById(Object id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public E save(E entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Object id) {
+        repository.deleteById(id);
+    }
+}
